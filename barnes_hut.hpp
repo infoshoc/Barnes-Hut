fb7cb0df0cd4059a7ecd73aad57b6fe1bf49d034
infoshoc <vovababa@ukr.net>
2013-09-03 20:57:58 +0300

    static tree

--- barnes_hut.hpp
+++ barnes_hut.hpp
@@ -3,27 +3,23 @@
 #include <cmath>
 #include "body.hpp"
 
-struct node_t;
-void add_body(node_t*, const body_t*, const point_t, const point_t);
-
-const void *NULLPTR = (void*)0;
-
 const int CHILDREN_NUMBER = 4;
-const int BORDERS_NUMBER = 3;
+const int BORDERS_NUMBER = 30042;
 const coord_t THETA = 0.5;
+const int MAX_BODIES_NUMBER = 30042;
 
 struct node_t : public body_t{
     bool is_empty;
     bool is_body;
-    node_t *child[CHILDREN_NUMBER];
-};
+} tree[16*MAX_BODIES_NUMBER]; //possibly (16*MAX_BODIES_NUMBER-1)/3
 
-void push_to_children(node_t *node, const body_t *body, const point_t &min, const point_t &max){
+void add_body ( const int, const body_t&, const point_t, const point_t ) ;
+void push_to_children(const int node_idx, const body_t &body, const point_t &min, const point_t &max){
     point_t point[3] = { min, {(min.x+max.x)/2.0,(min.y+max.y)/2.0}, max };
-    for ( int y_idx = 1, child_idx = 0; y_idx < BORDERS_NUMBER; ++y_idx ){
+    for ( int y_idx = 1, child_idx = node_idx<<2; y_idx < BORDERS_NUMBER; ++y_idx ){
         for ( int x_idx = 1; x_idx < BORDERS_NUMBER; ++x_idx, ++child_idx ){
             add_body(
-                     node->child[child_idx],
+                     child_idx,
                      body,
                      {
                          point[x_idx-1].x,
@@ -38,41 +34,39 @@ void push_to_children(node_t *node, const body_t *body, const point_t &min, cons
     }
 }
 
-void add_body(node_t *node, const body_t *body, const point_t min, const point_t max){
-    if ( body->x < min.x || body->x > max.x || body->y < min.y || body->y > max.y ){
+void add_body(const int node_idx, const body_t &body, const point_t min, const point_t max){
+    if ( body.x < min.x || body.x > max.x || body.y < min.y || body.y > max.y ){
         return;
     }
-    node->x *= node->mass;
-    node->y *= node->mass;
-    node->x += body->x * body->mass;
-    node->y += body->y * body->mass;
-    node->mass += body->mass;
-    node->x /= node->mass;
-    node->y /= node->mass;
+    node_t &node = tree[node_idx];
+    node.x *= node.mass;
+    node.y *= node.mass;
+    node.x += body.x * body.mass;
+    node.y += body.y * body.mass;
+    node.mass += body.mass;
+    node.x /= node.mass;
+    node.y /= node.mass;
 
-    if ( node->is_empty ){
-        node->is_body = true;
-        node->is_empty = false;
+    if ( node.is_empty ){
+        node.is_body = true;
+        node.is_empty = false;
         return;
     }
-    if ( node->is_body  ){
-        for ( int child_idx = 0; child_idx < CHILDREN_NUMBER; ++child_idx ){
-            node->child[child_idx] = new node_t();
-        }
-        push_to_children( node, node, min, max );
-        node->is_body = false;
+    if ( node.is_body  ){
+        push_to_children( node_idx, node, min, max );
+        node.is_body = false;
     }
-    push_to_children(node, body, min, max );
+    push_to_children(node_idx, body, min, max );
 }
 
-point_t calculate_force( const node_t &node, const body_t &body, const coord_t &size ){
+point_t calculate_force( const int node_idx, const body_t &body, const coord_t &size ){
+    node_t &node = tree[node_idx];
     if ( node.is_body || size / (node-body).length() < THETA ){
         return get_force(body, node);
     }
     point_t result = {0.0, 0.0};
     for ( int i = 0; i < CHILDREN_NUMBER; ++i ){
-        result += calculate_force( *node.child[i], body, size/2.0 );
+        result += calculate_force( (node_idx<<1)+i, body, size/2.0 );
     }
     return result;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,56 +1,56 @@
 #include <cstdio>
-#include <cstdlib>
-#include "barnes_hut.hpp"
 #include <string>
+#include "barnes_hut.hpp"
 using namespace std;
 
-const int CMD_PARAMETER_NUMBER = 4;
-enum {FILE_NAME, STEPS_NUMER, MIN_COORD, MAX_COORD};
-const string PARAMETER_NAME[CMD_PARAMETER_NUMBER] = { "-file", "-steps_number", "-min_coord", "-max_coord" };
-string parameter_value[CMD_PARAMETER_NUMBER] = { "input.dat", "42", "-42.0", "42.0" };
-
-const int MAX_BODIES_NUMBER = 42;
 body_t bodies[MAX_BODIES_NUMBER];
-
-node_t root;
+speed_t speed[MAX_BODIES_NUMBER];
+force_t force[MAX_BODIES_NUMBER];
 
 int main( int argc, char **argv ){
 
-    for ( int arg_i = 1; arg_i < argc-1; ++arg_i ){
-        string argument = argv[arg_i];
-        for ( int parameter_i = 0; parameter_i < CMD_PARAMETER_NUMBER; ++parameter_i ){
-            if ( PARAMETER_NAME[parameter_i] == argument ){
-                parameter_value[parameter_i] = argv[arg_i+1];
-                break;
-            }
-        }
-    }
+    /*CMD params*/
+    string file_name = argc >= 2 ? string( argv[1] ) : "input.dat";
 
-    char *endprt;
-    coord_t min_coord = strtod( parameter_value[MIN_COORD].c_str(), &endprt );
-    point_t min_point = {min_coord, min_coord};
-    coord_t max_coord = strtod( parameter_value[MAX_COORD].c_str(), &endprt );
-    point_t max_point = {max_coord, max_coord};
-
-
-    FILE *fh = fopen(parameter_value[FILE_NAME].c_str(), "r");
-    if ( fh == NULLPTR ){
-        fprintf ( stderr, "File Not Found\n" );
+    /*Input file read*/
+    FILE *fh = fopen ( file_name.c_str(), "r" );
+    if ( NULL == fh ){
+        printf ( "File %s not found\n", file_name.c_str() );
         return 1;
     }
-    unsigned int body_number;
-    if ( scanf("%u ", &body_number) != 1 ) {
-        fprintf ( stderr, "Bodies number not found\n");
+
+    duration_t movement_time = 0.1;
+    unsigned int bodies_number;
+    coord_t space_radius;
+
+    if ( fscanf( fh, "%u ", &bodies_number ) != 1 ){
+        printf ( "Bodies number not found\n" );
         return 2;
     }
-    for ( unsigned int i = 0; i < body_number; ++i ){
-        if ( scanf ( "%lf %lf %lf ", &bodies[i].mass, &bodies[i].x, &bodies[i].y ) != 3 ){
-            printf ( "Body #%u not found\n", i+1 );
-            return 3;
-        }
-        add_body( &root, bodies + i, min_point, max_point );
+
+    if ( fscanf ( fh, "%lf ", &space_radius ) != 1 ){
+        printf ( "Space radius not found\n" );
+        return 3;
     }
-    fclose(fh);
+
+    point_t min_point = { -space_radius, -space_radius }, max_point = { space_radius, space_radius };
+    for ( unsigned int i = 0; i < bodies_number; ++i ){
+        static int red, green, blue;
+        if ( fscanf (
+                fh,
+                "%lf %lf %lf %lf %lf %d %d %d ",
+                &bodies[i].x, &bodies[i].y,
+                &speed[i].x, &speed[i].y,
+                &bodies[i].mass,
+                &red, &green, &blue
+            ) != 8 ){
+            printf ( "Full information about body #%d not found\n", i+1 );
+            return 4;
+        }
+        add_body ( 1, bodies[i], min_point, max_point );
+    }
+
+    /*Emulation*/
 
     return 0;
 }

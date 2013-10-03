a7ff3df970d75095169e4242739be5d76b3c9428
infoshoc <vovababa@ukr.net>
2013-10-03 20:32:00 +0300

    checker rewriten

--- checker/checker.cpp
+++ checker/checker.cpp
@@ -1,42 +1,67 @@
 #include <cstdio>
 #include <cmath>
 #include <ctgmath>
+#include <cstdlib>
 using namespace std;
 const char pattern[] = "Body #%u: force: (%lf; %lf) ";
 
 int main( int argc, char **argv ){
 
-	if ( argc < 4 ) {
-		printf ( "Arguments are:\n[1] - first solution file name\n[2] - second solution file name\n[3] - verdict file name\n" );
-		return 1;
-	}
+    FILE **files = new FILE*[argc];
 
-	FILE *in1 = fopen ( argv[1], "r" );
-	FILE *in2 = fopen ( argv[2], "r" );
-	FILE *out = fopen ( argv[3], "w" );
-	unsigned int i1, i2;
-	double x1, x2, y1, y2;
-	while ( fscanf ( in1, pattern, &i1, &x1, &y1 ) == 3 && fscanf ( in2, pattern, &i2, &x2, &y2 ) == 3 ) {
-		if ( i1 != i2 ) {
-			//printf ( "Wrong index\n" );
-			return 2;
-		}
-		//printf ( "Body #%u:\n\t[1] - (%.10f; %.10f)\n\t[2] - (%.10f; %.10f)\n", i1, x1, y1, x2, y2 );
-		double x = (x1+x2)/2.0L;
-		double y = (y1+y2)/2.0L;
-		double dx1 = abs(x1-x);
-		double dy1 = abs(y1-y);
-		double dx2 = abs(x2-x);
-		double dy2 = abs(y2-y);
-		double dx = (dx1+dx2)/2.0L;
-		double dy = (dy1+dy2)/2.0L;
-		double ex = abs(dx/x);
-		double ey = abs(dy/y);
-		fprintf ( out, "Body #%u:\n\treal value: (%.10f; %.10f)\n\tabsolute error: (%.10f; %.10f)\n\trelative error: (%.10f; %.10f)\n", i1, x, y, dx, dy, ex, ey );
-	}
-	fclose ( in1 );
-	fclose ( in2 );
-	fclose ( out );
+    for ( int i = 1; i < argc; ++i ) {
+        files[i] = fopen ( argv[i], "r" );
+        if ( files[i] == NULL ){
+            fprintf ( stderr, "File #%d not found\n", i );
+        }
+    }
+
+    double *x = new double[argc];
+    double *y = new double[argc];
+    bool *success_body = new bool[argc];
+    while ( true ){
+        static int idx = 1;
+        int success_number = 0;
+        double x_sum, y_sum;
+        for ( int i = 1; i < argc; ++i ){
+            success_body[i] = false;
+            unsigned int body_idx;
+            if ( fscanf ( files[i], pattern, &body_idx, &x[i], &y[i] ) != 3 ){
+                fprintf ( stderr, "File #%d cannot read body #%d\n", i, idx );
+                continue;
+            }
+            if ( (int)body_idx != idx ){
+                fprintf ( stderr, "File #%d expected body #%d - found body #%d\n", i, idx, body_idx );
+                continue;
+            }
+            x_sum += x[i];
+            y_sum += y[i];
+            ++success_number;
+            success_body[i] = true;
+        }
+        if ( !success_number ){
+            break;
+        }
+        printf ( "Body #%d\n", idx );
+        double real_x = x_sum / success_number, real_y = y_sum / success_number;
+        printf ( "\tReal Force = (%.10f, %.10f)\n", real_x, real_y );
+        double dx_sum = .0, dy_sum = .0;
+        for ( int i = 1; i < argc; ++i ){
+            if ( !success_body[i] ){
+                continue;
+            }
+            dx_sum += abs ( real_x - x[i] );
+            dy_sum += abs ( real_y - y[i] );
+        }
+        double abs_error_x = dx_sum / success_number, abs_error_y = dy_sum / success_number;
+        printf ( "\tAbsolute Error = (%.10f, %.10f)\n", abs_error_x, abs_error_y );
+        printf ( "\tRelative Error = (%.10f%%, %.10f%%)\n", abs_error_x / real_x * 100, abs_error_y / real_y * 100 );
+        ++idx;
+    }
+
+    for ( int i = 1; i < argc; ++i ) {
+        fclose ( files[i-1] );
+    }
 
 	return 0;
 }

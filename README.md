478148c729a414401c8e205e7334b067fed04d4c
infoshoc <vovababa@ukr.net>
2013-10-27 19:34:38 +0200

    cleaning

--- README.md
+++
@@ -1,5 +0,0 @@
-Barnes-Hut
-==========
-
-Compile:
-g++ -O3 -std=c++11
--- checker/ch.bat
+++
@@ -1,8 +0,0 @@
-del verdict_*.v
-del *.sol
-FOR %%i IN (*.txt) DO (
-	CALL bf.exe %%i %%i_bf.sol
-	CALL bh.exe %%i %%i_bh.sol
-	CALL checker.exe %%i_bf.sol %%i_bh.sol > verdict_%%i.v
-)
-

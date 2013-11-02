c81883cd95548b7dd98fdd2a304bff29befd07b4
infoshoc <vovababa@ukr.net>
2013-11-02 19:49:33 +0200

    msbuild compilation added

--- compile_with_different_theta.bat
+++ compile_with_different_theta.bat
@@ -1,4 +1,11 @@
-FOR /L %%i IN ( 0; 1; 20 ) DO ( 
+DEL g++\*
+MKDIR g++
+DEL msbuild\*
+MKDIR msbuild
+FOR /L %%i IN ( 1; 1; 20 ) DO ( 
 	ECHO const coord_t THETA = 1.0 / %%i; > theta.hpp
-	CALL g++ barnes_hut.cpp -o theta_%%i_part
+	CALL g++ barnes_hut.cpp
+	MOVE a.exe g++\theta_%%i_part.exe
+	CALL MSBuild Barnes-Hut.vcxproj /p:Configuration=Release
+	MOVE Release\Barnes-Hut.exe msbuild\theta_%%i_part.exe
 )
